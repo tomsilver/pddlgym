9a56151d32d3cf0916a717e5e1b03d662db16928
Tom Silver <tom@Toms-MacBook-Pro-3.local>
2019-12-19 19:49:45 -0500

    wip

--- prolog_interface.py
+++ prolog_interface.py
@@ -1,50 +1,43 @@
 import subprocess
+import sys
 import tempfile
 
 
 class PrologInterface:
     """
     """
-    def __init__(self, kb, conds, max_assignment_count=2, allow_redundant_variables=True):
+    def __init__(self, kb, conds, max_assignment_count=2, timeout=2, 
+                 allow_redundant_variables=True):
         self._kb = kb
         self._conds = conds
         self._max_assignment_count = max_assignment_count
         self._allow_redundant_variables = allow_redundant_variables
-        self._check_format()
-        self._varnames_to_var = self._create_varname_to_var(self._conds, lower=True)
-        self._atomname_to_atom = self._create_varname_to_var(self._kb)
+        self._timeout = timeout
+        self._varnames_to_var = self._create_varname_to_var(self._conds, 
+            lambda x : self._clean_variable_name(x.lower()))
+        self._atomname_to_atom = self._create_varname_to_var(self._kb, self._clean_atom_name)
         self._prolog_str = self._create_prolog_str()
 
-    def _check_format(self):
-        """
-        """
-        # Make sure all atoms and literals are lowercase
-        for lit in self._kb:
-            assert not lit.is_negative, "Kb literals cannot be negative (closed world)"
-            if not lit.predicate.name.islower():
-                raise Exception("Predicate names must be lowercase for Prolog; {} is not.".format(lit.predicate.name))
-            for atom in lit.variables:
-                if not atom.islower():
-                    raise Exception("Atoms must be lowercase for Prolog; {} is not.".format(atom))
+    @classmethod
+    def _clean_atom_name(cls, atom_name):
+        return atom_name.lower().replace("-", "_")
 
-        # Make sure all variables are capitalized
-        for lit in self._conds:
-            if not lit.predicate.name.islower():
-                raise Exception("Predicate names must be lowercase for Prolog; {} is not.".format(lit.predicate.name))
-            for variable in lit.variables:
-                if variable.islower():
-                    raise Exception("Variables must be capitalized for Prolog; {} is not.".format(variable))
+    @classmethod
+    def _clean_variable_name(cls, var_name):
+        return var_name.replace("-", "_").replace("?", "")
+
+    @classmethod
+    def _clean_predicate_name(cls, predicate_name):
+        return predicate_name.lower().replace("-", "_")
 
     @staticmethod
-    def _create_varname_to_var(lits, lower=False):
+    def _create_varname_to_var(lits, transformer):
         """
         """
         vname_to_v = {}
         for lit in lits:
             for v in lit.variables:
-                vname = v.name
-                if lower:
-                    vname = vname.lower()
+                vname = transformer(v.name)
                 if vname in vname_to_v:
                     assert vname_to_v[vname] == v
                 else:
@@ -61,57 +54,60 @@ class PrologInterface:
         end = self._prolog_end(variables, self._max_assignment_count)
         return '\n'.join([preamble, kb_str, type_str, goal_str, end])
 
-    @staticmethod
-    def _prolog_kb_str(kb):
+    @classmethod
+    def _prolog_kb_str(cls, kb):
         """
         """
         kb_str = ""
         for lit in sorted(kb):
-            pred_name = lit.predicate.name
-            atoms = ",".join([a.name for a in lit.variables])
+            pred_name = cls._clean_predicate_name(lit.predicate.name)
+            atoms = ",".join([cls._clean_atom_name(a) for a in lit.variables])
             kb_str += "\n{}({}).".format(pred_name, atoms)
         return kb_str
 
-    @staticmethod
-    def _prolog_type_str(kb):
+    @classmethod
+    def _prolog_type_str(cls, kb):
         """
         """
         all_atoms = sorted({ v for lit in kb for v in lit.variables })
         type_str = ""
         for v in sorted(all_atoms, key=lambda v:v.var_type):
-            type_str += "\nistype{}({}).".format(v.var_type, v.name)
+            vname = cls._clean_atom_name(v.name)
+            type_str += "\nistype{}({}).".format(v.var_type, vname)
         return type_str
 
-    @staticmethod
-    def _prolog_goal(conds, allow_redundant_variables):
+    @classmethod
+    def _prolog_goal(cls, conds, allow_redundant_variables):
         """
         """
         all_vars = sorted({ v for lit in conds for v in lit.variables })
+        all_vars_cleaned = [cls._clean_variable_name(v) for v in all_vars]
         main_cond_str = ""
         for lit in sorted(conds):
-            pred_name = lit.predicate.name
+            pred_name = cls._clean_predicate_name(lit.predicate.name)
             if lit.is_negative:
                 pred_name = "\\+"+pred_name
-            variables = ",".join([a.name for a in lit.variables])
+            variables = ",".join([cls._clean_variable_name(a.name) for a in lit.variables])
             main_cond_str += "\n\t{}({}),".format(pred_name, variables)
         type_cond_str = ""
         for v in sorted(all_vars, key=lambda v:v.var_type):
-            type_cond_str += "\n\tistype{}({}),".format(v.var_type, v.name)
+            type_cond_str += "\n\tistype{}({}),".format(v.var_type, cls._clean_variable_name(v.name))
         if not allow_redundant_variables:
-            all_different_str = "\n\tall_different([{}]).".format(",".join(all_vars))
+            all_different_str = "\n\tall_different([{}]).".format(",".join(all_vars_cleaned))
         else:
             type_cond_str = type_cond_str[:-1]
             all_different_str = "."
-        head_str = "\ngoal({}) :-".format(",".join(all_vars))
+        head_str = "\ngoal({}) :-".format(",".join(all_vars_cleaned))
         final_str = head_str + main_cond_str + type_cond_str + all_different_str
         return final_str, all_vars
 
-    @staticmethod
-    def _prolog_preamble(conds):
+    @classmethod
+    def _prolog_preamble(cls, conds):
         pred_definitions = ""
         preds = sorted({ lit.predicate for lit in conds })
         for pred in preds:
-            pred_definitions += "\n:- multifile({}/{}).".format(pred.name, pred.arity)
+            pred_name = cls._clean_predicate_name(pred.name)
+            pred_definitions += "\n:- multifile({}/{}).".format(pred_name, pred.arity)
 
         return """print_solutions([]).
 print_solutions([H|T]) :- write(H), nl, print_solutions(T).
@@ -119,10 +115,10 @@ print_solutions([H|T]) :- write(H), nl, print_solutions(T).
 {}
 """.format(pred_definitions)
     
-    @staticmethod
-    def _prolog_end(variables, max_assignment_count):
-        lowercase_vars = ",".join([v.lower() for v in variables])
-        uppercase_vars = ",".join(variables)
+    @classmethod
+    def _prolog_end(cls, variables, max_assignment_count):
+        lowercase_vars = ",".join([cls._clean_variable_name(v.lower()) for v in variables])
+        uppercase_vars = ",".join([cls._clean_variable_name(v.capitalize()) for v in variables])
         return """
 :- use_module(library(bounds)).
 :- initialization (
@@ -145,8 +141,12 @@ print_solutions([H|T]) :- write(H), nl, print_solutions(T).
         tmp_name = file.name
         with open(tmp_name, 'w') as f:
             f.write(self._prolog_str)
-        cmd_str = "swipl {}".format(tmp_name)
+        timeout_str = "gtimeout" if sys.platform == 'darwin' else "timeout"
+        cmd_str = "{} {} swipl {}".format(timeout_str, self._timeout, tmp_name)
         output = subprocess.getoutput(cmd_str)
+        if "ERROR" in output:
+            import ipdb; ipdb.set_trace()
+            raise Exception("Prolog terminated with an error: \n{}".format(output))
         lines = output.split('\n')
         varnames = self._parse_output_line(lines.pop(0))
         vs = [self._varnames_to_var[v] for v in varnames]
@@ -161,5 +161,3 @@ print_solutions([H|T]) :- write(H), nl, print_solutions(T).
             assignment = dict(zip(vs, atoms))
             assignments.append(assignment)
         return assignments
-
-
