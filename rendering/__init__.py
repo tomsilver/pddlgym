28314d94380805eb0516b39af5f9f287c05b1407
Tom Silver <tom@Toms-MacBook-Pro-3.local>
2019-12-22 17:53:56 -0500

    doors rendering

--- rendering/__init__.py
+++ rendering/__init__.py
@@ -1,3 +1,4 @@
 from .minecraft import render as minecraft_render
 from .rearrangement import render as rearrangement_render
 from .sokoban import render as sokoban_render
+from .doors import render as doors_render
