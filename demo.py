fb6f6b26360618ce4cf9099469970a40eb956a49
Tom Silver <tom@Toms-MacBook-Pro-3.local>
2020-01-03 09:38:21 -0500

    make hanoi training problems smaller

--- demo.py
+++ demo.py
@@ -41,7 +41,7 @@ def run_all(render=True, verbose=True):
     # demo_ff_planning("casino", 1, render=render, verbose=verbose)
     # demo_ff_planning("casino", 4, render=render, test=True, verbose=verbose)
     demo_ff_planning("hanoi", 4, render=render, verbose=verbose)
-    demo_ff_planning("hanoi", 4, render=render, test=True, verbose=verbose)
+    demo_ff_planning("hanoi", 6, render=render, test=True, verbose=verbose)
 
 if __name__ == '__main__':
     run_all()
