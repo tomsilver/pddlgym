1db1ee56f972b3afecd6141309d7c2661c7d6831
Tom Silver <tom@Toms-MacBook-Pro-3.local>
2020-02-26 13:13:45 -0500

    reverting more files

--- pddlgym/rendering/__init__.py
+++ pddlgym/rendering/__init__.py
@@ -1,4 +1,8 @@
 from .minecraft import render as minecraft_render
 from .rearrangement import render as rearrangement_render
 from .sokoban import render as sokoban_render
+from .hanoi import render as hanoi_render
+from .blocks import render as blocks_render
 from .doors import render as doors_render
+from .tsp import render as tsp_render
+from .slidetile import render as slidetile_render
