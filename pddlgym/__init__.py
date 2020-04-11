a033d1e4429000913c482a77d905dc82948de1ee
Tom Silver <tom@Toms-MacBook-Pro-3.local>
2020-04-11 15:47:55 -0400

    allow operators as actions

--- pddlgym/__init__.py
+++ pddlgym/__init__.py
@@ -6,7 +6,7 @@ from gym.envs.registration import register
 
 import os
 
-def register_pddl_env(name, render, is_test_env, other_args):
+def register_pddl_env(name, is_test_env, other_args):
     dir_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "pddl")
     domain_file = os.path.join(dir_path, "{}.pddl".format(name.lower()))
     gym_name = name.capitalize()
@@ -20,28 +20,30 @@ def register_pddl_env(name, render, is_test_env, other_args):
         id='PDDLEnv{}-v0'.format(gym_name),
         entry_point='pddlgym.core:PDDLEnv',
         kwargs=dict({'domain_file' : domain_file, 'problem_dir' : problem_dir,
-                     'render' : render}, **other_args),
+                     **other_args}),
     )
 
-for env_name, render_fn in [
-        ("rearrangement", rearrangement_render),
-        ("sokoban", sokoban_render),
-        ("minecraft", minecraft_render),
-        ("depot", None),
-        ("baking", None),
-        ("blocks", blocks_render),
-        ("travel", None),
-        ("doors", doors_render),
-        ("casino", None),
-        ("hanoi", hanoi_render),
-        ("tsp", tsp_render),
-        ("slidetile", slidetile_render),
-        ("elevator", None),
-        ("ferry", None),
-        ("meetpass", None),
-        ("easyblocks", blocks_render),
-        ("conditionalblocks", blocks_render),
-        ("conditionalferry", None),
+for env_name, kwargs in [
+        ("rearrangement", {'render' : rearrangement_render}),
+        ("sokoban", {'render' : sokoban_render}),
+        ("minecraft", {'render' : minecraft_render}),
+        ("depot", {}),
+        ("baking", {}),
+        ("blocks", {'render' : blocks_render}),
+        ("travel", {}),
+        ("doors", {'render' : doors_render}),
+        ("casino", {}),
+        ("hanoi", {'render' : hanoi_render}),
+        ("tsp", {'render' : tsp_render}),
+        ("slidetile", {'render' : slidetile_render}),
+        ("elevator", {}),
+        ("ferry", {}),
+        ("meetpass", {}),
+        ("easyblocks", {'render' : blocks_render}),
+        ("conditionalblocks", {'render' : blocks_render}),
+        ("conditionalferry", {}),
+        ("blocks_operator_actions", {'render' : blocks_render, 
+                                     'operators_as_actions' : True}),
 ]:
     other_args = {
         "raise_error_on_invalid_action": False,
@@ -49,5 +51,6 @@ for env_name, render_fn in [
         "compute_approx_reachable_set": False,
         "shape_reward_mode": None,
     }
+    kwargs.update(other_args)
     for is_test in [False, True]:
-        register_pddl_env(env_name, render_fn, is_test, other_args)
+        register_pddl_env(env_name, is_test, kwargs)
