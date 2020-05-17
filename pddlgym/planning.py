5956f557e376672337708c3a88e9fe50715e2315
gehring <clement.gehring@gmail.com>
2020-05-17 12:10:18 -0400

    Fixed some formatting

--- pddlgym/planning.py
+++ pddlgym/planning.py
@@ -5,22 +5,28 @@ import re
 import subprocess
 import pyperplan
 
+
 class PlanningException(Exception):
     pass
+
 
 def run_planner(domain_file, problem_file, planner_name, **kwargs):
     if planner_name == 'ff':
         return run_ff(domain_file, problem_file, **kwargs)
     raise Exception("Unknown planner `{}`".format(planner_name))
 
+
 def run_ff(domain_file, problem_file, horizon=np.inf, timeout=10):
     if 'FF_PATH' not in os.environ:
-        raise Exception("Environment variable `FF_PATH` not found. " + \
-            "Make sure ff is installed and FF_PATH is set to the ff executable.")
+        raise Exception((
+            "Environment variable `FF_PATH` not found. Make sure ff is installed "
+            "and FF_PATH is set to the ff executable."
+        ))
 
     FF_PATH = os.environ['FF_PATH']
     timeout_cmd = "gtimeout" if sys.platform == "darwin" else "timeout"
-    cmd_str = "{} {} {} -o {} -f {}".format(timeout_cmd, timeout, FF_PATH, domain_file, problem_file)
+    cmd_str = "{} {} {} -o {} -f {}".format(timeout_cmd, timeout, FF_PATH,
+                                            domain_file, problem_file)
     output = subprocess.getoutput(cmd_str)
 
     if "goal can be simplified to FALSE" in output:
@@ -37,12 +43,16 @@ def run_ff(domain_file, problem_file, horizon=np.inf, timeout=10):
 
 def get_fd_optimal_plan_cost(domain_file, problem_file, timeout=10):
     if 'FD_PATH' not in os.environ:
-        raise Exception("Environment variable `FD_PATH` not found. " + \
-            "Make sure fd is installed and FD_PATH is set to the directory containing the fast-downward.py executable.")
+        raise Exception((
+            "Environment variable `FD_PATH` not found. Make sure fd is installed "
+            "and FD_PATH is set to the directory containing the fast-downward.py "
+            "executable."
+        ))
 
     FD_PATH = os.environ['FD_PATH']
     timeout_cmd = "gtimeout" if sys.platform == "darwin" else "timeout"
-    cmd_str = "{} {} {}/fast-downward.py --alias seq-opt-lmcut {} {}".format(timeout_cmd, timeout, FD_PATH, domain_file, problem_file)
+    cmd_str = "{} {} {}/fast-downward.py --alias seq-opt-lmcut {} {}".format(
+        timeout_cmd, timeout, FD_PATH, domain_file, problem_file)
     output = subprocess.getoutput(cmd_str)
 
     if "Solution found!" not in output:
