b2545179731df0e5d1a7165bf51e320ba881f608
Rohan Chitnis <ronuchit@mit.edu>
2020-06-22 10:48:56 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='pddlgym',
+      version='0.0.1',
+      install_requires=['matplotlib', 'pillow', 'gym', 'imageio', 'pyperplan'],
+      packages=find_packages(),
+)
+
