7ffb4bcf149fc72e8160f96f4707950434b5517c
Rohan Chitnis <ronuchit@mit.edu>
2020-06-22 10:50:52 -0400

    adding MANIFEST.in -- adding pddl files and images

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(name='pddlgym',
       version='0.0.1',
       install_requires=['matplotlib', 'pillow', 'gym', 'imageio', 'pyperplan'],
       packages=find_packages(),
+      include_package_data=True,
 )
 
