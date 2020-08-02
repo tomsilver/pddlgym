98bdc503df9ac1804be2dd121a858f8822fab36a
Tom Silver <tom@Toms-MacBook-Pro-3.local>
2020-08-02 14:35:49 -0400

    change to logistics problems

--- pddlgym/procedural_generation/hoffmann_logistics/script.sh
+++ pddlgym/procedural_generation/hoffmann_logistics/script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 for i in {0..39}
 do
-   ./logistics -a 50 -c 50 -s 2 -p 20 > ../../pddl/manylogistics/problem$i.pddl
+   ./logistics -a 10 -c 10 -s 2 -p 20 > ../../pddl/manylogistics/problem$i.pddl
 done
 
-for i in {40..49}
-do
-   ./logistics -a 50 -c 50 -s 2 -p 20 > ../../pddl/manylogistics_test/problem$i.pddl
-done
+# for i in {40..49}
+# do
+#    ./logistics -a 50 -c 50 -s 2 -p 20 > ../../pddl/manylogistics_test/problem$i.pddl
+# done
