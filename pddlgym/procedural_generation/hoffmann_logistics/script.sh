4d6be3f3ae15171a7e1853c110d984282d266e0e
Tom Silver <tom@Toms-MacBook-Pro-3.local>
2020-08-04 08:23:18 -0400

    simplifying logistics problems

--- pddlgym/procedural_generation/hoffmann_logistics/script.sh
+++ pddlgym/procedural_generation/hoffmann_logistics/script.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 for i in {0..39}
 do
-   ./logistics -a 10 -c 10 -s 2 -p 20 > ../../pddl/manylogistics/problem$i.pddl
+    let "AIRS=2+($RANDOM%5)"
+    let "CARS=2+($RANDOM%5)"
+    let "CITY=1+($RANDOM%3)"
+    let "PACKS=1+($RANDOM%8)"
+   ./logistics -a $AIRS -c $CARS -s $CITY -p $PACKS > ../../pddl/manylogistics/problem$i.pddl
+   echo "Generated logistics problem with $AIRS airplanes, $CARS cars, $CITY size cities, $PACKS packages."
 done
 
 # for i in {40..49}
