efb8a370e8c157e1806390d060d9eec17ad3fbb4
Tom Silver <tomssilver@gmail.com>
2020-09-04 11:15:39 -0400

    version 0.0.2 release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pddlgym',
-      version='0.0.1',
+      version='0.0.2',
       install_requires=['matplotlib', 'pillow', 'gym', 'imageio'],
       packages=find_packages(),
       include_package_data=True,
