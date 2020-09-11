a59c0cf67b952d1cb0f1c6d399b29ad60afcbd36
Tom Silver <tomssilver@gmail.com>
2020-09-11 07:11:53 -0400

    adding scikit-image to required packages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='pddlgym',
       version='0.0.2',
-      install_requires=['matplotlib', 'pillow', 'gym', 'imageio'],
+      install_requires=['matplotlib', 'pillow', 'gym', 'imageio', 'scikit-image'],
       packages=find_packages(),
       include_package_data=True,
 )
