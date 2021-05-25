57b3e324e31348fb6e0596cd74ef6daf60b1af38
Tom Silver <tomssilver@gmail.com>
2021-05-25 17:56:25 -0400

    preparing for version 0.0.4 release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pddlgym',
-      version='0.0.3',
+      version='0.0.4',
       install_requires=['matplotlib', 'pillow', 'gym', 'imageio', 'scikit-image'],
       packages=find_packages(),
       include_package_data=True,
