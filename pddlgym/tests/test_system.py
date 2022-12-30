d54202a4decb81c832ab0c1d55b47e8111214159
Michael Fishman <michaeljfishman@gmail.com>
2022-12-29 19:46:55 -0500

    Convert tests to unittest

--- pddlgym/tests/test_system.py
+++ pddlgym/tests/test_system.py
@@ -3,11 +3,15 @@ from pddlgym.demo import run_all
 import gym
 import pddlgym
 
-def test_system():
-    print("WARNING: this test may take around a minute...")
-    run_all(render=False, verbose=False)
-    print("Test passed.")
+import unittest
+
+
+class TestSystem(unittest.TestCase):
+    def test_system(self):
+        print("WARNING: this test may take around a minute...")
+        run_all(render=False, verbose=False)
+        print("Test passed.")
 
 
 if __name__ == '__main__':
-    test_system()
+    unittest.main()
