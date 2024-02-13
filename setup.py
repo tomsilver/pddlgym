870f9ac6ef77876ac7ab02e595248096a1e7cd84
Tom Silver <tomssilver@gmail.com>
2024-02-13 15:01:55 -0500

    move dependencies into setup.py and bump version

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(name='pddlgym',
-      version='0.0.6',
-      install_requires=['matplotlib', 'pillow', 'gym', 'imageio', 'scikit-image'],
+      version='0.0.7',
+      install_requires=[
+            'matplotlib',
+            'pillow>=8,<10',
+            'gym>=0.24.0',
+            'imageio',
+            'networkx',
+            'scikit-image',
+      ],
       packages=find_packages(),
       include_package_data=True,
 )
-
